d8ec43bbb7db4411a5efbc4497c0824dc0e9f0c9
KirillDemyanenko <demyanenkokg@gmail.com>
2022-10-14 11:09:36 +0300

    src/vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,81 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(typeOfMachine = true) {
+        this.machineType = typeOfMachine;
+    }
+
+    encrypt(message, key) {
+        if (!(arguments.length !== 2 || typeof arguments[0] === 'undefined' || typeof arguments[1] === 'undefined')) {
+            let b = 0
+        }
+        else {
+            throw new Error('Incorrect arguments!')
+        }
+        let resultString = '';
+        var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Array.prototype.insert = function (index, item) {
+            this.splice(index, 0, item);
+        };
+        let tempArray = message.split('')
+        let message1 = '';
+        for (let item of tempArray) {
+            if ((item.toUpperCase()).match(/[A-Z]/i)) {
+                message1 += item.toUpperCase()
+            }
+        }
+        key = (key.repeat(Math.ceil(message1.length / key.length))).slice(0, message1.length)
+        for (let i = 0; i < message1.length; i++) {
+            resultString += alphabet[((26 + alphabet.indexOf(message1[i].toUpperCase()) + alphabet.indexOf(key[i].toUpperCase())) % 26)]
+        }
+        tempArray = resultString.split('')
+        for (let i = 0; i < message.length; i++) {
+            if (!(message[i].toUpperCase()).match(/[A-Z]/i)) {
+                tempArray.insert(i, message[i])
+            }
+        }
+        if (this.machineType) {
+            return tempArray.join('')
+        } else {
+            return (tempArray.reverse()).join('')
+        }
+    }
+
+    decrypt(message, key) {
+        if (!(arguments.length !== 2 || typeof arguments[0] === 'undefined' || typeof arguments[1] === 'undefined')) {
+            let b = 0
+        }
+        else {
+            throw new Error('Incorrect arguments!')
+        }
+        let resultString = '';
+        var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Array.prototype.insert = function (index, item) {
+            this.splice(index, 0, item);
+        };
+        let tempArray = message.split('')
+        let message1 = '';
+        for (let item of tempArray) {
+            if ((item.toUpperCase()).match(/[A-Z]/i)) {
+                message1 += item.toUpperCase()
+            }
+        }
+        key = (key.repeat(Math.ceil(message1.length / key.length))).slice(0, message1.length)
+        for (let i = 0; i < message1.length; i++) {
+            resultString += alphabet[((26 + alphabet.indexOf(message1[i].toUpperCase()) - alphabet.indexOf(key[i].toUpperCase())) % 26)]
+        }
+        tempArray = resultString.split('')
+        for (let i = 0; i < message.length; i++) {
+            if (!(message[i].toUpperCase()).match(/[A-Z]/i)) {
+                tempArray.insert(i, message[i])
+            }
+        }
+        if (this.machineType) {
+            return tempArray.join('')
+        } else {
+            return (tempArray.reverse()).join('')
+        }
+    }
 }
 
 module.exports = {
